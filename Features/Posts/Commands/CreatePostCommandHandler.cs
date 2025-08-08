e2c493a667906583e62ded7e6c150ec78d9dc0a8
Ciorro <julzal2000@gmail.com>
2025-08-08 19:55:26 +0200

    Update CreatePostCommandHandler.cs

--- Features/Posts/Commands/CreatePostCommandHandler.cs
+++ Features/Posts/Commands/CreatePostCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using LiteBus.Commands.Abstractions;
 using YssWebstoreApi.Entities;
+using YssWebstoreApi.Features.Auth;
 using YssWebstoreApi.Persistance.Repositories.Interfaces;
 using YssWebstoreApi.Utils;
 
@@ -9,21 +10,38 @@ namespace YssWebstoreApi.Features.Posts.Commands
         : ICommandHandler<CreatePostCommand, Result<Guid>>
     {
         private readonly IRepository<Post> _postRepository;
+        private readonly IRepository<Project> _projectRepository;
         private readonly TimeProvider _timeProvider;
 
-        public CreatePostCommandHandler(IRepository<Post> postRepository, TimeProvider timeProvider)
+        public CreatePostCommandHandler(IRepository<Post> postRepository, IRepository<Project> projectRepository, TimeProvider timeProvider)
         {
             _postRepository = postRepository;
+            _projectRepository = projectRepository;
             _timeProvider = timeProvider;
         }
 
         public async Task<Result<Guid>> HandleAsync(CreatePostCommand message, CancellationToken cancellationToken = default)
         {
+            if (message.TargetProjectId.HasValue)
+            {
+                var project = await _projectRepository.GetAsync(message.TargetProjectId.Value);
+                
+                if (project == null)
+                {
+                    return CommonErrors.ResourceNotFound;
+                }
+                if (project.AccountId != message.AccountId)
+                {
+                    return AuthErrors.AccessDenied;
+                }
+            }
+
             var creationDate = _timeProvider.GetUtcNow().UtcDateTime;
+            var id = Guid.CreateVersion7(creationDate);
 
             var post = new Post()
             {
-                Id = Guid.CreateVersion7(),
+                Id = id,
                 CreatedAt = creationDate,
                 UpdatedAt = creationDate,
                 AccountId = message.AccountId,
