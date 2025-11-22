73ad8f70027df4e49fa6d07d940b28ae9df09d48
Ciorro <julzal2000@gmail.com>
2025-11-22 21:57:46 +0100

    Update UpdateAccountRequest.cs

--- Api/DTO/Accounts/UpdateAccountRequest.cs
+++ Api/DTO/Accounts/UpdateAccountRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace YssWebstoreApi.Api.DTO.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YssWebstoreApi.Api.DTO.Accounts
 {
     public class UpdateAccountRequest
     {
+        [Length(1, 80)]
         public required string DisplayName { get; set; }
+
+        [MaxLength(250)]
         public string? StatusText { get; set; }
     }
 }
