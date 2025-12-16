fa05441df25a36b5d8e7e7c1e2df2408e9a9a82c
Ciorro <julzal2000@gmail.com>
2025-12-16 22:46:01 +0100

    Updated project DTOs.

--- Api/DTO/Projects/CreateAssetRequest.cs
+++ Api/DTO/Projects/CreateAssetRequest.cs
@@ -4,12 +4,21 @@ namespace YssWebstoreApi.Api.DTO.Projects
 {
     public class CreateAssetRequest : CreateProjectRequest
     {
+        public required string AssetType { get; set; }
+        public required string Style { get; set; }
+        public List<string>? Themes { get; set; }
+
         public override TagCollection GetTags()
         {
-            return new TagCollection()
+            var tags = new TagCollection()
             {
-                new Tag("type", "asset")
+                new Tag("type", "asset"),
+                new Tag("asset", AssetType),
+                new Tag("style", Style)
             };
+
+            tags.AddValuesToGroup("theme", Themes ?? []);
+            return tags;
         }
     }
 }
--- Api/DTO/Projects/UpdateToolRequest.cs
+++ Api/DTO/Projects/UpdateToolRequest.cs
@@ -4,11 +4,14 @@ namespace YssWebstoreApi.Api.DTO.Projects
 {
     public class UpdateToolRequest : UpdateProjectRequest
     {
+        public required string ToolType { get; set; }
+
         public override TagCollection GetTags()
         {
             return new TagCollection()
             {
-                new Tag("type", "tool")
+                new Tag("type", "tool"),
+                new Tag("tool", ToolType)
             };
         }
     }
